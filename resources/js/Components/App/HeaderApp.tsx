adcaaea4a6903d3ce8e94d532dd623ecdf5a281b
mphstar <bintang.simdig20@gmail.com>
2024-11-22 22:01:43 +0700

    feat: slicing page user materi

--- resources/js/Components/App/HeaderApp.tsx
+++ resources/js/Components/App/HeaderApp.tsx
@@ -10,7 +10,7 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const HeaderApp = () => {
     return (
-        <nav className="px-4 py-4 flex justify-between items-center max-w-7xl mx-auto w-full">
+        <nav className="px-4 py-4 flex justify-between items-center max-w-7xl mx-auto w-full sticky top-0 z-[100] bg-[#F1F7FD] dark:bg-[#131414]">
             <div className="flex items-center">
                 <span className="text-2xl font-bold text-cyan-500">GO</span>
                 <span className="text-2xl font-bold text-gray-800 dark:text-white">
@@ -23,17 +23,20 @@ const HeaderApp = () => {
                 </div>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <div className="flex gap-2 items-center hover:bg-gray-200 px-3 py-2 rounded-md cursor-pointer">
+                        <div className="flex gap-2 items-center hover:bg-gray-200 dark:hover:bg-gray-800 px-3 py-2 rounded-md cursor-pointer">
                             <p>Mphstar</p>
                             <IoIosArrowDown />
                         </div>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end" className="w-56 p-2 shadow-none flex flex-col rounded-2xl">
-                        <div className="flex gap-2 items-center hover:bg-gray-100 px-3 py-2 rounded-md cursor-pointer">
+                    <DropdownMenuContent
+                        align="end"
+                        className="w-56 p-2 shadow-none flex flex-col rounded-2xl"
+                    >
+                        <div className="flex gap-2 items-center hover:bg-gray-100 dark:hover:bg-gray-800 px-3 py-2 rounded-md cursor-pointer">
                             <LuUser className="" />
                             <p className="text-sm">Akun Saya</p>
                         </div>
-                        <div className="flex gap-2 items-center hover:bg-gray-100 px-3 py-2 rounded-md cursor-pointer">
+                        <div className="flex gap-2 items-center hover:bg-gray-100 dark:hover:bg-gray-800 px-3 py-2 rounded-md cursor-pointer">
                             <IoExitOutline className="" />
                             <p className="text-sm">Keluar</p>
                         </div>
--- resources/js/Pages/Users/Materi.tsx
+++ resources/js/Pages/Users/Materi.tsx
@@ -7,8 +7,12 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/Components/ui/breadcump";
+import { Button } from "@/Components/ui/button";
 import { Input } from "@/Components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/Components/ui/tabs";
+import { Link } from "@inertiajs/react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FiBookOpen } from "react-icons/fi";
 
 const Materi = () => {
     return (
@@ -18,7 +22,7 @@ const Materi = () => {
                 <Breadcrumb>
                     <BreadcrumbList>
                         <BreadcrumbItem>
-                            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                            <Link href="/app/home">Home</Link>
                         </BreadcrumbItem>
                         <BreadcrumbSeparator />
                         <BreadcrumbItem>
@@ -27,7 +31,8 @@ const Materi = () => {
                     </BreadcrumbList>
                 </Breadcrumb>
             </div>
-            <div className="max-w-7xl mx-auto w-full px-4 mt-6">
+
+            <div className="max-w-7xl mx-auto w-full px-4 mt-6 ">
                 <h1 className="font-semibold text-xl">Materi Belajar</h1>
                 {/* add description */}
                 <p className="text-gray-500 dark:text-gray-400 text-sm">
@@ -43,49 +48,70 @@ const Materi = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-7xl mx-auto px-4 mt-6">
-                <div className="flex flex-col bg-background overflow-hidden group">
-                    <img
-                        className="w-full rounded-md h-64 object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out"
-                        src="/assets/images/book.jpg"
-                        alt="Book"
-                    />
-                    <div className="px-4 py-2 pb-6">
-                        <p className="font-medium mt-2">Materi Belajar</p>
-                        <p className="text-gray-500 dark:text-gray-300 text-sm">
-                            Rangkuman materi pembelajaran
-                        </p>
-                    </div>
+            <div className="sticky left-0 top-16 mt-4 z-[88] bg-[#F1F7FD] dark:bg-[#131414] px-4 max-w-7xl w-full mx-auto">
+                <Tabs
+                    defaultValue="Tes Wawasan Kebangsaan"
+                    className="flex flex-wrap bg-transparent p-0"
+                >
+                    <TabsList className="bg-transparent w-full md:w-fit">
+                        <TabsTrigger
+                            className="data-[state=active]:text-cyan-500 data-[state=active]:bg-transparent data-[state=active]:font-semibold w-full data-[state=active]:border-b-4 border-cyan-500"
+                            value="Tes Wawasan Kebangsaan"
+                        >
+                            <span className="block md:hidden">TWK</span>
+                            <span className="hidden md:block">
+                                Tes Wawasan Kebangsaan
+                            </span>
+                        </TabsTrigger>
+                        <TabsTrigger
+                            className="data-[state=active]:text-cyan-500 data-[state=active]:bg-transparent data-[state=active]:font-semibold w-full data-[state=active]:border-b-4 border-cyan-500"
+                            value="Tes Intelegensi Umum"
+                        >
+                            <span className="block md:hidden">TIU</span>
+                            <span className="hidden md:block">
+                                Tes Intelegensi Umum
+                            </span>
+                        </TabsTrigger>
+                        <TabsTrigger
+                            className="data-[state=active]:text-cyan-500 data-[state=active]:bg-transparent data-[state=active]:font-semibold w-full data-[state=active]:border-b-4 border-cyan-500"
+                            value="Tes Karakteristik Pribadi"
+                        >
+                            <span className="block md:hidden">TKP</span>
+                            <span className="hidden md:block">
+                                Tes Karakteristik Pribadi
+                            </span>
+                        </TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto w-full px-4 mt-6">
+                <CardMateri />
+                <CardMateri />
+                <CardMateri />
+                <CardMateri />
+            </div>
+        </div>
+    );
+};
+
+const CardMateri = () => {
+    return (
+        <div className="flex flex-col border-[2px] p-4 bg-background w-full rounded-md">
+            <div className="flex flex-row gap-2 items-center">
+                <div className="w-8 h-8 p-2 flex justify-center items-center rounded-md bg-cyan-500">
+                    <FiBookOpen className="text-white" />
                 </div>
-                <div className="flex flex-col bg-background overflow-hidden group">
-                    <img
-                        className="w-full rounded-md h-64 object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out"
-                        src="/assets/images/quiz.jpg"
-                        alt="Quiz"
-                    />
-                    <div className="px-4 py-2 pb-6">
-                        <p className="font-medium mt-2">Try Out</p>
-                        <p className="text-gray-500 dark:text-gray-300 text-sm">
-                            Tantang diri kamu untuk meraih skor tertinggi dari
-                            pengguna lain.
-                        </p>
-                    </div>
-                </div>
-                <div className="flex flex-col bg-background overflow-hidden group">
-                    <img
-                        className="w-full rounded-md h-64 object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out"
-                        src="/assets/images/history.jpg"
-                        alt="History"
-                    />
-                    <div className="px-4 py-2 pb-6">
-                        <p className="font-medium mt-2">Riwayat Kuis</p>
-                        <p className="text-gray-500 dark:text-gray-300 text-sm">
-                            Lihat dan pelajari kembali jawaban try out yang
-                            pernah kamu kerjakan
-                        </p>
-                    </div>
+                <div>
+                    <h1 className="font-semibold">Nama Materi</h1>
+                    <p className="text-sm text-gray-500">
+                        Tes Wawasan Kebangsaan
+                    </p>
                 </div>
             </div>
+            <Button className="mt-4 bg-cyan-100 hover:bg-cyan-200 text-cyan-600">
+                Lihat Materi
+            </Button>
         </div>
     );
 };
